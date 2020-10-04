659149662af669a1ba27a204aceaf22ce087719c
bclswl0827 <bclswl0827@gmail.com>
2020-10-04 22:03:27 +0800

    decryption: none

--- configure.sh
+++ configure.sh
@@ -23,7 +23,8 @@ cat << EOF > /usr/local/etc/v2ray/config.json
                     {
                         "id": "$UUID"
                     }
-                ]
+                ]，
+				"decryption": "none"
             },
             "streamSettings": {
                 "network": "ws"
