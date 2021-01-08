0e1396f7d6b6fc63f959786d73fcb21ac21cd89c
adskd <772552412@qq.com>
2021-01-08 13:00:22 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -24,7 +24,7 @@ cat << EOF > /usr/local/etc/v2ray/config.json
                         "id": "$UUID"
                     }
                 ],
-				"decryption": "none"
+		  "decryption": "none"
             },
             "streamSettings": {
                 "network": "ws"
